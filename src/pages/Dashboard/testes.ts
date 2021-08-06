494b71e1d26bf58148885a6977d61c9acb44be00
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-08-06 00:54:55 -0300

    Update testes.ts

--- src/pages/Dashboard/testes.ts
+++ src/pages/Dashboard/testes.ts
@@ -1,5 +1,4 @@
-
-
+const value = 'texto';
 const booleano: boolean = true; 
 
 const object = {
@@ -20,7 +19,44 @@ function sum(primeiroNumero: number, segundoNumero: number): number{
   return primeiroNumero + segundoNumero
 }
 
-sum(1, 2)
-
 const obj: object = {} // any object
 const str = '';
+
+interface User {
+  nome: string;
+  idade?: string;
+}
+
+function printaUsuario(user: User){
+  console.log(user.nome)
+  console.log(user.idade)
+
+  return { peso: 75, altura: 175, cidade: 'Fortaleza' }
+};
+
+interface UserGithub {
+  login: string;
+}
+
+// Partial
+function printarGithubUsername(user: Partial<UserGithub>){
+  console.log(user);
+}
+
+//Pick
+function printarGithubPick(user: Pick<UserGithub, 'followers' | 'following'>){
+  console.log(user.followers);
+  console.log(user.following);
+}
+printarGithubPick({ followers: '', following: ''})
+
+//Omit
+function printarGithubOmit(user: Omit<UserGithub, 'followers'>){
+  console.log(user.company);
+}
+printarGithubOmit({ company: ''})
+
+//Readonly
+const newGithubUsername: UserGithub = {
+
+}
