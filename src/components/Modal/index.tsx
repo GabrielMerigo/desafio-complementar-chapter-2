7fae701c3b9868e9ebbc955ec26885b28a69f2f3
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-08-10 20:33:44 -0300

    testes

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,48 +1,45 @@
-import { ReactNode } from 'react';
-import { useEffect, useState } from 'react';
-import ReactModal from 'react-modal';
+import { useState, useEffect } from "react";
+import ReactModal from "react-modal";
 
-interface ModalProps {
+type ModalProps = {
   isOpen: boolean;
-  children: ReactNode;
-  setIsOpen: () => void;
-}
+  setIsOpen(): void;
+  children: React.ReactNode;
+};
 
-function Modal({ isOpen, children, setIsOpen }: ModalProps) {
-  const [modalStatus, setModalStatus] = useState(isOpen)
+function Modal(props: ModalProps) {
+  const [modalStatus, setModalStatus] = useState(props.isOpen);
 
-  // useEffect((prevProps) => {
-  //   if (prevProps.isOpen !== isOpen) {
-  //     setModalStatus(isOpen)
-  //   }
-  // })
+  useEffect(() => {
+    setModalStatus(props.isOpen);
+  }, [props.isOpen]);
 
   return (
     <ReactModal
       shouldCloseOnOverlayClick={!false}
-      onRequestClose={setIsOpen}
+      onRequestClose={props.setIsOpen}
       isOpen={modalStatus}
       ariaHideApp={false}
       style={{
         content: {
-          top: '50%',
-          left: '50%',
-          right: 'auto',
-          bottom: 'auto',
-          marginRight: '-50%',
-          transform: 'translate(-50%, -50%)',
-          background: '#F0F0F5',
-          color: '#000000',
-          borderRadius: '8px',
-          width: '736px',
-          border: 'none',
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          background: "#F0F0F5",
+          color: "#000000",
+          borderRadius: "8px",
+          width: "736px",
+          border: "none",
         },
         overlay: {
-          backgroundColor: '#121214e6',
+          backgroundColor: "#121214e6",
         },
       }}
     >
-      {children}
+      {props.children}
     </ReactModal>
   );
 }
--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,45 +1,44 @@
-import { createRef, useState } from 'react';
-import { FiCheckSquare } from 'react-icons/fi';
-import { Foods } from '../../pages/Dashboard';
+import { useRef } from "react";
+import { FiCheckSquare } from "react-icons/fi";
+import { FormHandles } from "@unform/core";
 
-import { Form } from './styles';
-import Modal from '../Modal';
-import Input from '../Input';
+import { Form } from "./styles";
+import Modal from "../Modal";
+import Input from "../Input";
+import { FoodType } from "../Food";
 
-interface ModalAddFoodProps {
-  setIsOpen: () => void;
-  handleAddFood: (food: Foods) => Promise<void>;
+type ModalAddFoodProps = {
   isOpen: boolean;
-}
+  setIsOpen(): void;
+  handleAddFood(food: FoodType): void;
+};
 
-function ModalAddFood({ handleAddFood, setIsOpen, isOpen}:ModalAddFoodProps) {
-  async function handleSubmit(data: Foods) {
+function ModalAddFood(props: ModalAddFoodProps) {
+  const formRef = useRef<FormHandles>(null);
 
-    handleAddFood(data);
-    setIsOpen();
-  };
+  async function handleSubmit(data: FoodType) {
+    props.handleAddFood(data);
+    props.setIsOpen();
+  }
 
-  // ref={this.formRef}
   return (
-    <>
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form onSubmit={handleSubmit}> 
-          <h1>Novo Prato</h1>
-          <Input name="image" placeholder="Cole o link aqui" />
+    <Modal isOpen={props.isOpen} setIsOpen={props.setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
 
-          <Input name="name" placeholder="Ex: Moda Italiana" />
-          <Input name="price" placeholder="Ex: 19.90" />
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
 
-          <Input name="description" placeholder="Descrição" />
-          <button type="submit" data-testid="add-food-button">
-            <p className="text">Adicionar Prato</p>
-            <div className="icon">
-              <FiCheckSquare size={24} />
-            </div>
-          </button>
-        </Form>
-      </Modal>
-    </>
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
   );
 }
 
